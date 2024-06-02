8c83e33fcaf02d516d7aad147b88a6a8a83628c9
HugoClemt <hugoclemt@gmail.com>
2024-06-02 20:53:43 +0200

    Authentification ( signup & signin ) with redirection HomeScreen

---
+++ lib/view/login_screen.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+import 'package:projet_mds/service/authentification_service.dart';
+import 'package:projet_mds/view/home_screen.dart';
+import 'package:projet_mds/view/register_screen.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  final TextEditingController _usernameController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  final AuthentificationService _authentificationservice =
+      AuthentificationService();
+  String? _message;
+
+  void _login() async {
+    final username = _usernameController.text;
+    final password = _passwordController.text;
+
+    final response = await _authentificationservice.login(username, password);
+
+    if (response.success) {
+      if (response.token != null) {
+        await _authentificationservice.saveToken(response.token!);
+      }
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => const HomeScreen()),
+      );
+    } else {
+      setState(() {
+        _message = response.message;
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Login'), actions: [
+        ElevatedButton(
+            onPressed: () {
+              Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => const Registerscreen()));
+            },
+            child: const Text('Sign Up')),
+      ]),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            TextField(
+              controller: _usernameController,
+              decoration: const InputDecoration(labelText: 'Username'),
+            ),
+            TextField(
+              controller: _passwordController,
+              decoration: const InputDecoration(labelText: 'Password'),
+              obscureText: true,
+            ),
+            if (_message != null) Text(_message!),
+            ElevatedButton(
+              onPressed: _login,
+              child: const Text('Login'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/view/register_screen.dart
@@ -0,0 +1,101 @@
+import 'package:flutter/material.dart';
+import 'package:projet_mds/service/authentification_service.dart';
+import 'package:projet_mds/view/home_screen.dart';
+import 'package:projet_mds/view/login_screen.dart';
+
+class Registerscreen extends StatefulWidget {
+  const Registerscreen({super.key});
+
+  @override
+  State<Registerscreen> createState() => _RegisterscreenState();
+}
+
+class _RegisterscreenState extends State<Registerscreen> {
+  final TextEditingController _usernameController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _firstnameController = TextEditingController();
+  final TextEditingController _lastnameController = TextEditingController();
+  final AuthentificationService _authentificationservice =
+      AuthentificationService();
+  String? _message;
+
+  void _register() async {
+    final String username = _usernameController.text;
+    final String password = _passwordController.text;
+    final String email = _emailController.text;
+    final String firstname = _firstnameController.text;
+    final String lastname = _lastnameController.text;
+
+    final response = await _authentificationservice.register(
+        username, password, email, firstname, lastname);
+
+    if (response.success) {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => const HomeScreen()),
+      );
+    } else {
+      setState(() {
+        _message = response.message;
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Inscription'),
+        actions: [
+          ElevatedButton(
+              onPressed: () {
+                Navigator.pushReplacement(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => const LoginScreen()));
+              },
+              child: const Text('Sign In')),
+        ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          children: <Widget>[
+            if (_message != null)
+              Container(
+                alignment: Alignment.center,
+                child: Text(_message!),
+              ),
+            TextField(
+              controller: _usernameController,
+              decoration:
+                  const InputDecoration(labelText: 'Nom d\'utilisateur'),
+            ),
+            TextField(
+              controller: _passwordController,
+              decoration: const InputDecoration(labelText: 'Mot de passe'),
+              obscureText: true,
+            ),
+            TextField(
+              controller: _emailController,
+              decoration: const InputDecoration(labelText: 'Email'),
+            ),
+            TextField(
+              controller: _firstnameController,
+              decoration: const InputDecoration(labelText: 'Prénom'),
+            ),
+            TextField(
+              controller: _lastnameController,
+              decoration: const InputDecoration(labelText: 'Nom'),
+            ),
+            ElevatedButton(
+              onPressed: _register,
+              child: const Text('S\'inscrire'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
